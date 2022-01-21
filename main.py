5f398b416727e256bad5ceded82ca63899e47ea1
JoeCP17 <at365@naver.com>
2022-01-21 22:11:47 +0900

    크롤링 작업 일부진행

--- main.py
+++ main.py
@@ -2,24 +2,37 @@
 1. 이미지 크롤링을 진행하여 해당 이미지들을 먼저 전달하기
 2. DB에 저장할 내용들 크롤링 진행
 """
-
-import requests
+import os
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
+
+image_folder = './book_image'
+
+if not os.path.isdir(image_folder):
+    os.mkdir(image_folder)
+else:
+    print('해당 폴더가 존재합니다.')
 
 
 def image_get():
     driver = webdriver.Chrome('./chromedriver')
     driver.get('http://www.kyobobook.co.kr/categoryRenewal/categoryMain.laf?linkClass=0101&mallGb=KOR&orderClick=JAR')
-    for i in range(1,40,2):
-        elem = driver.find_element_by_xpath(f'//*[@id="prd_list_type1"]/li[{i}]/div/div[1]/div[2]/div[1]/a')  # 각 path 접근
+    for i in range(1, 40, 2):
+        elem = driver.find_element_by_xpath(
+            f'//*[@id="prd_list_type1"]/li[{i}]/div/div[1]/div[2]/div[1]/a')  # 각 path 접근
         elem.click()  # 접근한 path 타이틀 클릭 (상세페이지 접속)
-        time.sleep(2)
-        book_image = driver.find_element_by_xpath('//*[@id="container"]/div[2]/form/div[2]/div[1]/div/a/img') # 책 이미지를 크게 저장하기위한 이미지 클릭
-        book_image.click()
-        time.sleep(2) #여기까진 성공
+        time.sleep(3)
+
+        book_image_botton = driver.find_element_by_xpath(
+            '//*[@id="container"]/div[2]/form/div[2]/div[1]/div/a/img')  # 책 이미지를 크게 저장하기위한 이미지 클릭
+        book_image_botton.click()
+        time.sleep(3)  # 여기까진 성공
+
+        book_url = driver.find_element_by_xpath('/html/body/table/tbody/tr/td/table[2]/tbody/tr/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td[2]/table/tbody/tr[1]/td[1]/table/tbody/tr/td/img').get_attribute('src')  # 책 이미지 path url 가져오기
+        count = 1  # 책 이름 (가제)
+        urllib.request.urlretrieve(book_url, str(count) + ".jpg")  # 이미지 저장 근데 왜안됨...
+        time.sleep(3)
 
 
 image_get()
