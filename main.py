c1f3da476bdafe74f9e2182ac905f44ab2e4f26d
JoeCP17 <at365@naver.com>
2022-01-22 17:51:44 +0900

    이미지 수집까지 완성

--- main.py
+++ main.py
@@ -18,28 +18,25 @@ else:
 def image_get():
     driver = webdriver.Chrome('./chromedriver')
     driver.get('http://www.kyobobook.co.kr/categoryRenewal/categoryMain.laf?linkClass=0101&mallGb=KOR&orderClick=JAR')
-    for i in range(1, 40, 2):
-        elem = driver.find_element_by_xpath(
-            f'//*[@id="prd_list_type1"]/li[{i}]/div/div[1]/div[2]/div[1]/a')  # 각 path 접근
-        elem.click()  # 접근한 path 타이틀 클릭 (상세페이지 접속)
-        time.sleep(3)
+    elem = driver.find_element_by_xpath(
+            '//*[@id="prd_list_type1"]/li[1]/div/div[1]/div[2]/div[1]/a')  # 각 path 접근
+    elem.click()  # 접근한 path 타이틀 클릭 (상세페이지 접속)
+    time.sleep(3)
 
-        book_image_botton = driver.find_element_by_xpath(
+    book_image_botton = driver.find_element_by_xpath(
             '//*[@id="container"]/div[2]/form/div[2]/div[1]/div/a/img')  # 책 이미지를 크게 저장하기위한 이미지 클릭
-        book_image_botton.click()
-        time.sleep(3)  # 여기까진 성공
+    book_image_botton.click()
+    time.sleep(3)  # 여기까진 성공
 
-        book_url = driver.find_element_by_xpath(
-            '/html/body/table/tbody/tr/td/table[2]/tbody/tr/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td[2]/table/tbody/tr[1]/td[1]/table/tbody/tr/td/img').get_attribute(
-            'src')  # 책 이미지 path url 가져오기
-        count = 1  # 책 이름 (가제)
-        urllib.request.urlretrieve(book_url, str(count) + ".jpg")  # 이미지 저장 근데 왜안됨...
-        time.sleep(3)
+    driver.switch_to.frame('UXModalIframe') # 프레임 전환 하여 이미지 상세창으로 이동
 
-        back_botton = driver.find_element_by_xpath(
+    book_url = driver.find_element_by_xpath('/html/body/table/tbody/tr/td/table[2]/tbody/tr/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td[2]/table/tbody/tr[1]/td[1]/table/tbody/tr/td/img').get_attribute('src')
+    count = 1  # 책 이름 (가제)
+    urllib.request.urlretrieve(book_url, str(count) + ".jpg")  # 이미지 저장 근데 왜안됨...
+    time.sleep(3)
+    back_botton = driver.find_element_by_xpath(
             "/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[3]/a/img")  # x 버튼 이미지 path 탐색
-        back_botton.click()  # 해당 버튼 클릭
-        driver.back()  # 상세 페이지에서 뒤로가기
-
+    back_botton.click()  # 해당 버튼 클릭
+    driver.back()  # 상세 페이지에서 뒤로가기
 
 image_get()
